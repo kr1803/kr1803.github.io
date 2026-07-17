76299a47c7ecf817eb0e40e1a2a58020f5872f11
kr1803 <rikocainher@gmail.com>
2026-07-17 20:24:43 +0900

    Update text content in page4.js for localization

--- page4.js
+++ page4.js
@@ -5,28 +5,28 @@ const from = document.getElementById("from");
 
 setTimeout(()=>{
 
-line1.textContent="Happy 300 Days ❤️";
+line1.textContent="300일 축하햅 ❤️";
 line1.classList.add("show");
 
 },500);
 
 setTimeout(()=>{
 
-line2.innerHTML="300일 동안<br>내 곁에 있어줘서 고마워.";
+line2.innerHTML="300일 동안<br>내 곁에 있어줘서 고마워";
 line2.classList.add("show");
 
 },2000);
 
 setTimeout(()=>{
 
-line3.innerHTML="앞으로도<br>지금처럼<br>행복하자.";
+line3.innerHTML="앞으로도<br>지금처럼<br>행복하자~";
 line3.classList.add("show");
 
 },4000);
 
 setTimeout(()=>{
 
-from.innerHTML="From. 강린";
+from.innerHTML="From. 린";
 from.classList.add("show");
 
 },6500);
